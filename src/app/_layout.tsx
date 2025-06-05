2ac6570c6383535efad6195e545f7b0067e2c284
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-05 18:11:58 -0300

    Customizando o Drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,7 +6,34 @@ import { MaterialIcons } from "@expo/vector-icons";
 const LayoutRoot = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer />
+      <Drawer
+        screenOptions={{
+          drawerActiveTintColor: "#e91e63",
+          drawerInactiveTintColor: "#000",
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            title: "SignIn",
+            drawerLabel: "Home",
+            drawerIcon: ({ size, color }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Drawer.Screen
+          name="SignUp"
+          options={{
+            title: "SignUp",
+            drawerLabel: "Home",
+            drawerIcon: ({ size, color }) => (
+              <MaterialIcons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Drawer>
     </GestureHandlerRootView>
   );
 };
