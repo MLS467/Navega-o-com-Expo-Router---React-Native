c24f76505f3c6f9dbfcc5796612bc35a80f5167e
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-05 17:29:00 -0300

    Navegação do tipo tab

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,38 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
 const LayoutRoot = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        // tabBarShowLabel: false, // serve para esconder o nome do tab
+        // tabBarInactiveTintColor: "#888", // cor do ícone quando não está ativo
+        // tabBarActiveIntColor: "#000", // cor do ícone quando está ativo
+        tabBarLabelPosition: "beside-icon", // altera o posicionamento do texto dos ícones na tab
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "SignIn",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="house" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="signup"
+        options={{
+          title: "SignUp",
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons name="person" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 export default LayoutRoot;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,16 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { Button } from "@/components/Button";
 import { Link } from "expo-router";
 const Index = () => {
   return (
     <View style={style.container}>
       <Button title="Criar Conta" path="/signup" type="primary" />
-
-      <Link
-        style={{ color: "#000", paddingTop: 20 }}
-        href={{ pathname: "/home", params: { id: 50 } }}
-      >
-        Params
-      </Link>
     </View>
   );
 };
