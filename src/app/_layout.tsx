7eb8ece4331ed50d1dc07adb295d89d83618abd7
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-06-05 18:11:19 -0300

    Instalando o Drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,38 +1,13 @@
-import { Tabs } from "expo-router";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const LayoutRoot = () => {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        // tabBarShowLabel: false, // serve para esconder o nome do tab
-        // tabBarInactiveTintColor: "#888", // cor do ícone quando não está ativo
-        // tabBarActiveIntColor: "#000", // cor do ícone quando está ativo
-        tabBarLabelPosition: "beside-icon", // altera o posicionamento do texto dos ícones na tab
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "SignIn",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialIcons name="house" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="signup"
-        options={{
-          title: "SignUp",
-          tabBarIcon: ({ size, color }) => (
-            <MaterialIcons name="person" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer />
+    </GestureHandlerRootView>
   );
 };
 export default LayoutRoot;
